ab588d4a5a9a5b7d4934a43b3c28d6215b70f1c9
Marc Gagliardo <blackicehog@gmail.com>
2017-06-08 21:24:40 -0400

    Updated explanations for chap3ex4.c

--- cprimerplus/exercises/src/chap3/chap3ex4.c
+++ cprimerplus/exercises/src/chap3/chap3ex4.c
@@ -2,6 +2,7 @@
    exponential notation, and hexadecimal exponential notation (available in C99) */
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
 
 int main(void) {
 	double entry;
@@ -12,9 +13,15 @@ int main(void) {
 	   "warning: format specifies type 'float *' but the argument has type
       	   'double *' [-Wformat]".  Why is this not a problem when printing?*/
 	
-	/* Why is the "l" modifier needed before the %f format specifier? */	
+	/* Why is the "l" modifier needed before the %f format specifier? The "l"
+	   modifier is needed for the scanf function because it needs to know 
+	   that the pointer that was passed to it is actually a double */	
 
 	scanf("%lf", &entry);
+
+	/* "%lf" is not needed in the printf function but it can be used without
+	   error.  The reason is that printf is a variadic function, and there is 
+	   a "default argument promotion" to double */
 
 	printf("fixed point notation: %f\n", entry);
 	printf("exponential notation: %e\n", entry);
